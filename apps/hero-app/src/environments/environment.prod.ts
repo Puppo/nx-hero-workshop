7a26ae0125554f48537baf622915705675f39eee
Luca Del Puppo <luca@delpuppo.net>
2021-04-02 16:26:39 +0200

    deploy fe

--- apps/hero-app/src/environments/environment.prod.ts
+++ apps/hero-app/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
-export const environment = {
+import { Environment } from './environment.model';
+export const environment: Environment = {
   production: true,
+  baseApiUrl: 'https://5lsip2q1yd.execute-api.eu-central-1.amazonaws.com/prod',
 };
